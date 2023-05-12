3c10e2e316604a1b44903eb5efb99ba7bef951c1
bortw <pikanicka@gmail.cim>
2023-05-12 14:32:47 +0300

    Python для решения практических задач. 1. Обработка web-страниц. 1.2 Обработка html как текста

--- Python для решения практических задач/main.py
+++ Python для решения практических задач/main.py
@@ -22,3 +22,38 @@ else:
 
 
 # -------------------------------------------
+
+
+# ----------------- 1.2 Обработка html как текста --------------------
+
+# ------------ Task 1 -------------------
+# Файл https://stepik.org/media/attachments/lesson/209719/2.html содержит статью с Википедии про язык Python. В этой
+# статье есть теги code, которыми выделяются конструкции на языке Python. Вам нужно найти все строки, содержащиеся
+# между тегами <code> и </code> и найти те строки, которые встречаются чаще всего и вывести их в алфавитном
+# порядке, разделяя пробелами.
+#
+# Например, если исходный текст страницы выглядел бы так:
+#
+# <code>a</code>
+# <a>bracadabr</a>
+# <code>c</code>
+# <code>b</code>
+# <code>b</code>
+# <code>c</code>
+
+# то в ответ надо было бы ввести строку "b c".
+
+
+from collections import Counter
+from re import findall
+from urllib.request import urlopen
+html = urlopen("https://stepik.org/media/attachments/lesson/209719/2.html").read().decode('utf-8')
+text = str(html)
+regex = '<code>(.*?)</code>'
+l = sorted(findall(regex, text))
+l = Counter(l).most_common()
+res = list(filter(lambda x: x[1] == l[0][1], l))
+res = list(map(lambda x: x[0], res))
+print(' '.join(res))
+
+# ---------------------------------------
